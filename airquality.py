78c07e3b7594104214192b8332619ffebff880f1
tho-wa <tho.walther@gmail.com>
2020-04-25 22:01:31 +0200

    changed s3

--- airquality.py
+++ airquality.py
@@ -46,7 +46,7 @@ s3_client = boto3.client('s3')
 date = datetime.now()
 
 if len(bucket) > 0:
-    response = s3_client.put_object(Body=json.dumps(bucket), Bucket='sdd-s3-basebucket',
+    response = s3_client.put_object(Body=json.dumps(bucket), Bucket='sdd-s3-bucket',
                                     Key='airquality/{}/{}/{}/{}'.format(str(date.year).zfill(4),
                                                                         str(date.month).zfill(2),
                                                                         str(date.day).zfill(2),
